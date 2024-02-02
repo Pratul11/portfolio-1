a2c7ab3542f54a7a6f9ff06329e785c796acb760
Pratul Johari <pratuljohari608@gmail.com>
2024-02-02 21:40:40 +0530

    updated

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  var icon = document.getElementById("dark-icon");
+  icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+      icon.src = "./assests/sun.png";
+    }
+    else{
+      icon.src = "./assests/moon.png";
+    }
+  }
+  
