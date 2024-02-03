a88f555e6fe11e457e9fe945a7a7d9706b622984
Pratul Johari <pratuljohari608@gmail.com>
2024-02-03 19:49:24 +0530

    update dark mode

--- script.js
+++ script.js
@@ -14,4 +14,14 @@ function toggleMenu() {
       icon.src = "./assests/moon.png";
     }
   }
+  var icon = document.getElementById("dark-icon1");
+  icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+      icon.textContent = "Light Mode";
+    }
+    else{
+      icon.textContent = "Dark Mode";
+    }
+  }
   
